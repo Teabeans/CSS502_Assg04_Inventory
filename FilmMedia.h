53d293ef4562e6e31a91b5ff991c62456715e0c0
Tim Lum <twhlum@gmail.com>
2018-02-26 14:49:40 -0800

    Update FilmMedia.h

--- FilmMedia.h
+++ FilmMedia.h
@@ -1,1 +1,17 @@
-
+class Film:Media {
+   struct actorNode {
+      string actor;
+      actorNode* next;
+   }
+private:
+   string director;
+   actorNode* actorList;
+   string category;
+   string inventoryID;
+public:
+   bool featuresActor();
+   string getActor();
+   string getDirector();
+   void setActor();
+   void setDirector();
+};
