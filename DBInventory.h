4efc48d83fd8c0d85a4a88697404ba1a12791b3b
Tim Lum <twhlum@gmail.com>
2018-02-26 15:19:18 -0800

    Update DBInventory.h

--- DBInventory.h
+++ DBInventory.h
@@ -1,1 +1,11 @@
-
+class InventoryDB:Database {
+private:
+   film* inventoryTable[];
+   int estimateSize();
+   int hashTitle();
+   bool isValid();
+public:
+  void display();
+  void readIn();
+  string toString();
+};
