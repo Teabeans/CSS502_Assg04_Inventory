5234cbdb8438d830466bffbb9408c83ea5c5cfc0
Tim Lum <twhlum@gmail.com>
2018-02-26 14:43:41 -0800

    Update Media.h

--- Media.h
+++ Media.h
@@ -1,1 +1,18 @@
+class Media{
+private:
+   string title;
+   int stock;
+   int releaseYear;
+public:
+   string toString();
+   void display();
 
+   int getRelease();
+   int getStock();
+   string getTitle();
+
+   void setRelease();
+   void setStock();
+   void setTitle();
+   
+};
