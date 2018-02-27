0da0f5483dff1cc28ef90031ad50ee4d63fb68f7
Tim Lum <twhlum@gmail.com>
2018-02-26 16:09:09 -0800

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,1 +1,11 @@
-
+class Customer {
+private:
+   string firstName;
+   string lastName;
+   int IDnumber;
+   Transaction* history[];
+public:
+   int getID();
+   string getName();
+   string queryHistory();
+};
