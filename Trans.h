dee4dd7dd6954861eedf488bd9889a7c4f6210a9
Tim Lum <twhlum@gmail.com>
2018-02-28 16:48:00 -0800

    Update Trans.h

--- Trans.h
+++ Trans.h
@@ -1,8 +1,23 @@
-class Transaction {
-private:
-   string transactionID;
-   void errorOutput();
-   bool isValid();
+#pragma once
+#include <string>
+class Trans {
 public:
-   string toString()
+   std::string toString();
+   char getType();
+   std::string getInvID();
+   int getCustID();
+   int getQty();
+   void setInvID(std::string);
+   void setCustID(int);
+   void setQty(int);
+   Trans();
+   Trans(std::string);
+   ~Trans();
+private:
+   char type; // The transaction type
+   std::string invID; // The thing transacted
+   int customerID; // To whom it was transacted
+   int qty; // And the quantity of the transaction
+   std::string identifier1; //first identifier (eg. title or major actor)
+   std::string identifier2; //second identifier (eg. year or release date)
 };
