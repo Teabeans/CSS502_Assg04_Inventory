08d65c880f646408054e0bdd654effea44989e3d
Tim Lum <twhlum@gmail.com>
2018-03-03 18:26:18 -0800

    Update CustDB.h

--- Cust/CustDB.h
+++ Cust/CustDB.h
@@ -1,17 +1,126 @@
+//-------------------------------------|
+// Begin Student Code
+//-------------------------------------|
+
+// Requisite in the .h file
 #pragma once
+
+// Necessary for string operations
 #include <string>
+
+// Necessary for customer operations
 #include "Cust.h"
+
+// Necessary for transaction operations
 #include "../Trans/Trans.h"
 
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       CLASS CONSTANTS
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+// None for this class
+
 class CustDB {
-public:
-   std::string toString();
-   void display();
-   Cust* getCustomerAt(int);
-   void appendHistory(Trans);
-   CustDB();
-   ~CustDB();
+
 private:
-   Cust custTable[10000];
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PRIVATE FIELDS (-)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------|
+// #custTable
+//-----------------|
+// Desc:   The customer database
+// Invars: 10000 valid customer IDs, so 2x and nearest prime size
+   Cust custTable[20011];
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PRIVATE METHODS (-)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------|
+// #isValid
+//-----------------|
+// Desc:   Determines whether this string is a valid customerDB command
+// Invars: Return value is variable based on state of
    bool isValid(std::string);
-};
+
+public:
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PUBLIC FIELDS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+// None for this class
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PUBLIC METHODS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------|
+// #toString()
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   std::string toString();
+
+//-----------------|
+// #display()
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   void display();
+
+//-----------------|
+// #getCustomerAt(int)
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   Cust* getCustomerAt(int);
+
+//-----------------|
+// #appendHistory(Trans)
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   void appendHistory(Trans);
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       GETTERS / SETTERS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------|
+// #CustDB()
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   CustDB();
+
+//-----------------|
+// #~CustDB()
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+   ~CustDB();
+
+}; // Closing class CustDB {}
+
+// End of file - CustDB.h
