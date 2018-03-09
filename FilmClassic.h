c48967d67a4f24113371d7c1e5581d71441f3c94
Tim Lum <twhlum@gmail.com>
2018-03-09 00:34:26 -0800

    Create FilmClassic.h

---
+++ FilmClassic.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Film.h"
+
+class Classic : public Film {
+public:
+   std::string getReleaseDate(); // Override Film::getReleaseDate()
+   void setReleaseMonth(int);
+
+   Classic();
+   Classic(std::string);
+   ~Classic();
+private:
+   int releaseMonth;
+};
+// End of File - FilmClassic.h
