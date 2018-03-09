d6b9f5b5797b467f5f7f77d576238b98a65483f4
Tim Lum <twhlum@gmail.com>
2018-03-08 22:35:55 -0800

    Update InvDB.h

--- Inv/InvDB.h
+++ Inv/InvDB.h
@@ -1,34 +1,156 @@
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// UW CSS 502A - Assignment 4 - Inventory Management
+// InvDB.h
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------------------------------------------------------------------|
+// Authorship
+//-----------------------------------------------------------------------------|
+//
+// Matt Gross & Tim Lum
+// mattgrosspersonal@gmail.com + twhlum@gmail.com
+// Created:  2018.03.03
+// Modified: 2018.03.--
+// For the University of Washington Bothell, CSS 502A
+// Winter 2018, Graduate Certificate in Software Design & Development (GCSDD)
+//
+
+//-----------------------------------------------------------------------------|
+// File Description
+//-----------------------------------------------------------------------------|
+//
+// This is the header file for the InvDB class, representing a database of
+// film objects
+//
+
+//-----------------------------------------------------------------------------|
+// Package Files
+//-----------------------------------------------------------------------------|
+//
+// See Github repository
+// https://github.com/Teabeans/CSS502_Assg04_Inventory
+//
+
+//-------------------------------------|
+// Begin Student Code
+//-------------------------------------|
+
 #pragma once
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       INCLUDE STATEMENTS
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
 
 #include <iostream>
 #include <string>
 #include <vector>
-#include "../Trans/Trans.h"
+#include "Trans.h"
 #include "Film.h"
 #include "Classic.h"
 #include "Drama.h"
 #include "Comedy.h"
 
 class InvDB {
-public:
-   std::string toString();
-   void display();
-   void adjustStock(Trans&);
-   bool isLegal(std::string);
-   bool contains(Film*);
-   Film* retrieve(Film*);
-   bool addComedy(Comedy*);
-   bool addDrama(Drama*);
-   bool addClassic(Classic*);
-
-   InvDB();
-   InvDB(std::ifstream&);
-   ~InvDB();
 
 private:
-   std::vector<Comedy> comedies;
-   std::vector<Drama> dramas;
-   std::vector<Classic> classics;
-   bool isValid(std::string);
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PRIVATE FIELDS (-)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------|
+// #comedies
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+std::vector<Comedy> comedies;
+
+//-----------------|
+// #dramas
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+std::vector<Drama> dramas;
+
+//-----------------|
+// #classics
+//-----------------|
+// Desc:   NULL
+// Invars: NULL
+std::vector<Classic> classics;
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PRIVATE METHODS (-)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
    int hashTitle(std::string);
+
+   bool isValid(std::string);
+
+public:
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PUBLIC FIELDS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+// None for this class
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       PUBLIC METHODS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+   bool addClassic(Classic*);
+
+   bool addComedy(Comedy*);
+
+   bool addDrama(Drama*);
+
+   void adjustStock(Trans&);
+
+   bool contains(Film*);
+
+   void display();
+
+   bool isLegal(std::string);
+
+   Film* retrieve(Film*);
+
+   std::string toString();
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       GETTERS / SETTERS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+// None for this class
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       CONSTRUCTORS / DESTRUCTORS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+   InvDB();
+
+   InvDB(std::ifstream&);
+
+   ~InvDB();
+
+
 };
