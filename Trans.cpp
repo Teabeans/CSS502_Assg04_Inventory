a031c918a407ae2d9211edc19937faf9c897703e
Tim Lum <twhlum@gmail.com>
2018-03-09 23:46:59 -0800

    Update Trans.cpp

--- Trans.cpp
+++ Trans.cpp
@@ -117,13 +117,13 @@
 ///   std::string title;
 
 //-----------------|
-// #identifier1
-// #identifier2
+// #director
+// #actor
 //-----------------|
 // Desc:   Unique identifiers of the transaction
 // Invars: Initializes to junk values
-///   std::string identifier1; //first identifier (eg. title or major actor)
-///   std::string identifier2; //second identifier (eg. year or release date)
+///   std::string director;
+///   std::string actor;
 
 //-----------------|
 // #release
