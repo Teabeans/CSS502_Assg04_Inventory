7d30de73c6afaff524e03416000c1bb21690cd96
Tim Lum <twhlum@gmail.com>
2018-03-09 20:18:36 -0800

    Update Cust.h

--- Cust.h
+++ Cust.h
@@ -140,7 +140,7 @@ public:
 // Desc:   Appends values to the customer history
 // Invars: May be called with faulty transaction information
 //         GIGO - No error checking is performed by appendHistory()
-   void appendHistory(std::string someTransaction);
+   void appendHistory(std::string someTransaction, char type, std::string title);
 
 //-----------------|
 // #isCheckedOut(string)
