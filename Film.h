7aa4e3d92f9786e2e95022c8ed6933585738b42f
Tim Lum <twhlum@gmail.com>
2018-03-11 22:47:00 -0700

    Update Film.h

--- Film.h
+++ Film.h
@@ -132,6 +132,13 @@ public:
    // Desc:   Tests the film against another based on it's title & year
    // Invars: None
    virtual bool equals(Film&);
+   
+   // (+) --------------------------------|
+   // #isGreaterThan(Film&)
+   //-------------------------------------|
+   // Desc:   Tests the film against another based on it's title & year
+   // Invars: None
+   virtual bool isGreaterThan(Film&);
 
    // (+) --------------------------------|
    // #isLessThan(Film&)
@@ -141,116 +148,64 @@ public:
    virtual bool isLessThan(Film&);
 
    // (+) --------------------------------|
-   // #isGreaterThan(Film&)
+   // #makeFilm(string)
    //-------------------------------------|
-   // Desc:   Tests the film against another based on it's title & year
-   // Invars: None
-   virtual bool isGreaterThan(Film&);
+   // Desc:   Factory method; Returns a film of differing types based on input
+   // Invars: The command is legal
+   Film makeFilm(std::string command);
 
-   //-----------------|
-   // #getTitle()
-   //-----------------|
-   // Desc:   Returns the film's title
-   // Invars: None
-   std::string getTitle();
-
-   //-----------------|
-   // #setTitle(string)
-   //-----------------|
-   // Desc:   sets the film's title
-   // Invars: None
-   void setTitle(std::string);
-
-   //-----------------|
-   // #getStock()
-   //-----------------|
-   // Desc:   Returns the film's stock value
-   // Invars: None
-   int getStock();
-
-   //-----------------|
-   // #setStock(int)
-   //-----------------|
-   // Desc:   sets the film's stock value
-   // Invars: None
-   void setStock(int);
-
-   //-----------------|
-   // #getreleaseYear()
-   //-----------------|
-   // Desc:   Returns the film's release date
-   // Note:   Can't be Virtual as the return type for Classic differs, causing an error
-   // Invars: None
-   int getReleaseYear();
-
-   //-----------------|
-   // #setreleaseYear(int)
-   //-----------------|
-   // Desc:   sets the film's release date
-   // Invars: None
-   void setReleaseYear(int);
-
-   //-----------------|
-   // #getreleaseYear()
-   //-----------------|
-   // Desc:   NULL
-   // Note:   Overrides Film::getreleaseYear()
-   // Invars: NULL
-   int getReleaseMonth(); 
-
-   //-----------------|
-   // #setReleaseMonth()
-   //-----------------|
-   // Desc:   NULL
-   // Invars: NULL
-   void setReleaseMonth(int);
-
-   //-----------------|
-   // #getGenre()
-   //-----------------|
-   // Desc:   Returns the film's genre
-   // Invars: None
-   char getGenre();
-
-   //-----------------|
-   // #setGenre(char)
-   //-----------------|
-   // Desc:   sets the film's grenre (backup for the object type)
-   // Invars: None
-   void setGenre(char);
-
-   //-----------------|
-   // #getDirector()
-   //-----------------|
-   // Desc:   Returns the film's director
-   // Invars: None
-   std::string getDirector();
-
-   //-----------------|
-   // #setTitle(string)
-   //-----------------|
-   // Desc:   sets the film's title
-   // Invars: None
-   void setDirector(std::string);
-
-   //-----------------|
-   // #getActor()
-   //-----------------|
-   // Desc:   returns the film's major actor
-   // Invars: None
-   std::string getActor();
-
-   //-----------------|
-   // #setActor(string)
-   //-----------------|
-   // Desc:   sets the film's major actor
-   // Invars: None
-   void setActor(std::string);
 
 
 //-------|---------|---------|---------|---------|---------|---------|---------|
 //
-//       OPERATORS
+//       GETTERS / SETTERS (+)
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+   //-----------------|
+   // #getActor()
+   // #getDirector()
+   // #getGenre()
+   // #getreleaseMonth()
+   // #getreleaseYear()
+   // #getStock()
+   // #getTitle()
+   //-----------------|
+   // Desc:   Field accessors
+   // Invars: None
+   std::string getActor();
+   std::string getDirector();
+   char getGenre();
+   int getReleaseMonth(); 
+   int getReleaseYear();
+   int getStock();
+   std::string getTitle();
+
+
+   //-----------------|
+   // #setActor(string)
+   // #setDirector(string)
+   // #setGenre(char)
+   // #setReleaseMonth(int)
+   // #setreleaseYear(int)
+   // #setStock(int)
+   // #setTitle(string)
+   //-----------------|
+   // Desc:   Field setters
+   // Invars: None
+   void setActor(std::string);
+   void setDirector(std::string);
+   void setGenre(char);
+   void setReleaseMonth(int);
+   void setReleaseYear(int);
+   void setStock(int);
+   void setTitle(std::string);
+
+
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+//       OPERATOR OVERLOADS
 //
 //-------|---------|---------|---------|---------|---------|---------|---------|
 
@@ -294,7 +249,7 @@ public:
    //-----------------|
    // #Film(string)
    //-----------------|
-   // Desc:   NULL
+   // Desc:   Constructor that builds an appropriate Film subclass by string
    // Invars: NULL
    Film(std::string);
 
