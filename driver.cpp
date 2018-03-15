e800894244b0b38bbe25ff7b27df602358d44209
Tim Lum <twhlum@gmail.com>
2018-03-14 22:38:11 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -12,7 +12,7 @@
 // Matt Gross & Tim Lum
 // mattgrosspersonal@gmail.com + twhlum@gmail.com
 // Created:  2018.03.03
-// Modified: 2018.03.--
+// Modified: 2018.03.14
 // For the University of Washington Bothell, CSS 502A
 // Winter 2018, Graduate Certificate in Software Design & Development (GCSDD)
 //
@@ -1018,7 +1018,7 @@ void bulkReadTrans(std::ifstream& commandFile, CustDB& tgtCustDB, InvDB& tgtInvD
          // Otherwise...
          else {
             // Output the history of the customer at this position
-            std::cout << tgtCustDB.getCustomerAt(custID)->getHistory() << std::endl << std::endl;
+            std::cout << tgtCustDB.getCustomerAt(custID)->getHistory() << std::endl;
          }
       } // Closing if - 'H' condition handled
 
